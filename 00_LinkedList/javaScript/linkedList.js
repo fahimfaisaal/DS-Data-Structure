efdb765a0b5a9684426b2b046e8d62ea125891b6
fahim faisal <fahimfaisal.soikot@gmail.com>
2021-04-05 12:47:21 +0600

    modify some features

--- 00_LinkedList/javaScript/linkedList.js
+++ 00_LinkedList/javaScript/linkedList.js
@@ -1,118 +1,118 @@
-console.clear()
+console.clear();
 
 class LinkedList {
-    #list = new Object();
+  #list = new Object();
 
-    constructor(...data) {
-        this.data = typeof data[0] === 'object' ? data.flat() : data;
+  constructor(...data) {
+    this.data = typeof data[0] === "object" ? data.flat() : data;
+  }
+
+  #listGenerator(next = null) {
+    if (!this.data.length) {
+      return {
+        head: this.#list,
+      };
     }
 
-    #listGenerator(next = null) {
-        if (!this.data.length) {
-            return {
-                head: this.#list,
-            };
-        }
+    if (!next) this.#list.value = this.data.shift();
 
-        if (!next) this.#list.value = this.data.shift();
-      
-        return this.#listGenerator(
-            this.#list.next = {
-                value: this.data.pop(),
-                next: next,
-            }
-        );
+    return this.#listGenerator(
+      (this.#list.next = {
+        value: this.data.pop(),
+        next: next,
+      })
+    );
+  }
+
+  get getList() {
+    return this.#listGenerator();
+  }
+
+  #lengthGenerator(len = 0, node = this.getList.head) {
+    if (!node) {
+      return len;
     }
 
-    get getList() {
-        return this.#listGenerator();
+    return this.#lengthGenerator(len + 1, node.next);
+  }
+
+  get getLength() {
+    return this.#lengthGenerator();
+  }
+
+  get getFirstNode() {
+    return this.getList.head;
+  }
+
+  #lastNode(lastNode = this.getList.head) {
+    if (lastNode.next) {
+      return this.#lastNode(lastNode.next);
     }
 
-    #lengthGenerator(len = 0, node = this.getList.head) {
-        if (!node) {
-            return len;
-        }
+    return lastNode;
+  }
 
-        return this.#lengthGenerator(len + 1, node.next)
+  get getLastNode() {
+    return this.#lastNode();
+  }
+
+  set pushNode(data) {
+    if (!Object.keys(this.getList.head).length) {
+      this.getList.head.value = data;
+      this.getList.head.next = null;
+    } else
+      this.getLastNode.next = {
+        value: data,
+        next: null,
+      };
+  }
+
+  get popNode() {
+    const pop = {
+      ...this.getLastNode,
+    };
+
+    delete this.getLastNode.value;
+    delete this.getLastNode.next;
+
+    let node = this.getFirstNode;
+
+    while (Object.keys(node.next).length) {
+      node = node.next;
     }
 
-    get getLength() {
-        return this.#lengthGenerator()
+    node.next = null;
+
+    return pop;
+  }
+
+  clear() {
+    this.data.length = 0;
+  }
+
+  displayNodeValues(head = this.getList.head, nodes = "^-->") {
+    if (head.value) nodes += `${head.value}-->`;
+
+    if (!head.next) {
+      return nodes + "$";
     }
 
-    get getFirstNode() {
-        return this.getList.head;
-    }
+    return this.displayNodeValues(head.next, nodes);
+  }
 
-    #lastNode(lastNode = this.getList.head) {
-        if (lastNode.next) {
-            return this.#lastNode(lastNode.next)
-        }
-
-        return lastNode
-    }
-
-    get getLastNode() {
-        return this.#lastNode();
-    }
-
-   set pushNode(data) {
-        if (!Object.keys(this.getList.head).length) {
-            this.getList.head.value = data
-            this.getList.head.next = null
-        } else this.getLastNode.next = {
-              value: data,
-              next: null,
-        };
-    }
-
-   get popNode() {
-        const pop = {
-          ...this.getLastNode
-        };
-
-        delete this.getLastNode.value
-        delete this.getLastNode.next
-
-        let node = this.getFirstNode
-
-        while (Object.keys(node.next).length) {
-            node = node.next
-        }
-
-        node.next = null;
-
-        return pop
-    }
-    
-    clear() {
-        this.data.length = 0
-    }
-
-    displayNodeValues(head = this.getList.head, nodes = "^-->") {
-        if (head.value) nodes += `${head.value}-->`;
-
-        if (!head.next) {
-            return nodes + "$";
-        }
-
-        return this.displayNodeValues(head.next, nodes)
-    }
-
-    toString() {
-        return JSON.stringify(this.getList, null, 2);
-    }
-
+  toString() {
+    return JSON.stringify(this.getList, null, 2);
+  }
 }
 
-const linkedList = new LinkedList(1, 2, 3)
+const linkedList = new LinkedList(1, 2, 3);
 
-linkedList.pushNode = 4
-linkedList.pushNode = 5
-linkedList.pushNode = 6
+linkedList.pushNode = 4;
+linkedList.pushNode = 5;
+linkedList.pushNode = 6;
 
 console.log(linkedList.popNode);
-linkedList.popNode
+linkedList.popNode;
 
-console.log(linkedList.displayNodeValues())
-console.log(linkedList.toString())
+console.log(linkedList.displayNodeValues());
+console.log(linkedList.toString());

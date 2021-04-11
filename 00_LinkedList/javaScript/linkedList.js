fb28878fd3b2588aa3de3ec4d1e65aad006dc22c
fahim faisal <fahimfaisal.soikot@gmail.com>
2021-04-11 09:14:25 +0600

    modified

--- 00_LinkedList/javaScript/linkedList.js
+++ 00_LinkedList/javaScript/linkedList.js
@@ -1,118 +1,76 @@
 console.clear();
 
-class LinkedList {
-  #list = new Object();
-
-  constructor(...data) {
-    this.data = typeof data[0] === "object" ? data.flat() : data;
-  }
-
-  #listGenerator(next = null) {
-    if (!this.data.length) {
-      return {
-        head: this.#list,
-      };
-    }
-
-    if (!next) this.#list.value = this.data.shift();
-
-    return this.#listGenerator(
-      (this.#list.next = {
-        value: this.data.pop(),
-        next: next,
-      })
-    );
-  }
-
-  get getList() {
-    return this.#listGenerator();
-  }
-
-  #lengthGenerator(len = 0, node = this.getList.head) {
-    if (!node) {
-      return len;
-    }
-
-    return this.#lengthGenerator(len + 1, node.next);
-  }
-
-  get getLength() {
-    return this.#lengthGenerator();
-  }
-
-  get getFirstNode() {
-    return this.getList.head;
-  }
-
-  #lastNode(lastNode = this.getList.head) {
-    if (lastNode.next) {
-      return this.#lastNode(lastNode.next);
-    }
-
-    return lastNode;
-  }
-
-  get getLastNode() {
-    return this.#lastNode();
-  }
-
-  set pushNode(data) {
-    if (!Object.keys(this.getList.head).length) {
-      this.getList.head.value = data;
-      this.getList.head.next = null;
-    } else
-      this.getLastNode.next = {
-        value: data,
-        next: null,
-      };
-  }
-
-  get popNode() {
-    const pop = {
-      ...this.getLastNode,
-    };
-
-    delete this.getLastNode.value;
-    delete this.getLastNode.next;
-
-    let node = this.getFirstNode;
-
-    while (Object.keys(node.next).length) {
-      node = node.next;
-    }
-
-    node.next = null;
-
-    return pop;
-  }
-
-  clear() {
-    this.data.length = 0;
-  }
-
-  displayNodeValues(head = this.getList.head, nodes = "^-->") {
-    if (head.value) nodes += `${head.value}-->`;
-
-    if (!head.next) {
-      return nodes + "$";
-    }
-
-    return this.displayNodeValues(head.next, nodes);
-  }
-
-  toString() {
-    return JSON.stringify(this.getList, null, 2);
+class Node {
+  constructor(nodeData) {
+    this.data = nodeData;
+    this.next = null;
   }
 }
 
-const linkedList = new LinkedList(1, 2, 3);
+class SinglyLinkedList {
+  
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
 
-linkedList.pushNode = 4;
-linkedList.pushNode = 5;
-linkedList.pushNode = 6;
+  /**
+   * @param {any} nodeData
+   * @returns {Node}
+   */
+  set setNode(nodeData) {
+    const node = new Node(nodeData);
 
-console.log(linkedList.popNode);
-linkedList.popNode;
+    if (this.head === null) {
+      this.head = node;
+    } else {
+      this.tail.next = node;
+    }
 
-console.log(linkedList.displayNodeValues());
-console.log(linkedList.toString());
+    this.tail = node;
+  }
+
+  get getHead() {
+    return this.head;
+  }
+
+  get getTail() {
+    return this.tail;
+  }
+
+  /**
+   * @param  {Array<any>} data
+   * @returns {this}
+   */
+  insertNodes(...data) {
+    data = typeof data[0] === "object" ? data.flat() : data;
+
+    return data.reduce((list, value) => {
+      this.setNode = value;
+      return list;
+    }, this);
+  }
+
+  shiftNode() {
+    const nextNode = this.head.next;
+
+    if (nextNode) {
+      this.head = nextNode;
+    } else {
+      this.head = nextNode;
+      this.tail = nextNode;
+    }
+  }
+
+  /**
+   * @returns {this} in String format
+   */
+  toString() {
+    return JSON.stringify(this, null, 2);
+  }
+}
+
+const list = new SinglyLinkedList();
+
+list.insertNodes(1, 2, 3, 4, 5, 6);
+console.log(list.toString());

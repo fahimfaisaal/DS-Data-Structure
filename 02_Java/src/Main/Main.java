5ec02b5c7c0b4b8f9aa8c98c308bbd2cd7c5657c
fahim faisal <fahimfaisal.soikot@gmail.com>
2021-04-23 22:25:33 +0600

    change on main

--- 02_Java/src/Main/Main.java
+++ 02_Java/src/Main/Main.java
@@ -2,12 +2,13 @@ package Main;
 
 import java.util.Scanner;
 import LinkedList.SinglyLinkedList.*;
+import Stack.*;
 
 public class Main {
 
     public static void main(String[] args) {
 	     // Singly linkedList
-        Scanner scan = new Scanner(System.in);
+        /*Scanner scan = new Scanner(System.in);
 
         SinglyLinkedList singlyLinkedList = new SinglyLinkedList();
 
@@ -20,7 +21,25 @@ public class Main {
         SinglyNode secondLast = singlyLinkedList.pop();
 
         System.out.println(singlyLinkedList.toString());
-        System.out.println("last node = " + lastNode + '\n' + "second last = " + secondLast);
+        System.out.println("last node = " + lastNode + '\n' + "second last = " + secondLast);*/
+
+        // Stack
+        Stack stack = new Stack();
+
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+
+        StackNode lastNode = stack.pop();
+        StackNode lastSecond = stack.pop();
+        StackNode lastThird = stack.pop();
+
+
+        stack.push(4);
+        stack.push(5);
+
+        System.out.println(stack.toString());
+        System.out.println("last node = " + lastNode + "second last = " + lastSecond + "third last = " + lastThird);
 
     }
 }

8229f1723b4005427988e0db5f35be933f703f3c
fahim faisal <fahimfaisal.soikot@gmail.com>
2021-04-26 23:26:13 +0600

    add iterator method

--- 03_JavaScript/02_Stack/Stack.js
+++ 03_JavaScript/02_Stack/Stack.js
@@ -81,12 +81,30 @@ class Stack {
         return lastNode;
     }
 
+    
+    iterator() {
+        let iterate = this.getHead;
+        
+        return {
+            next: () => {
+                const node = iterate;
+                
+                iterate = node !== null ? iterate.next : this.getHead;
+                
+                return {
+                    done: node === null,
+                    data: node ? node.data : null
+                }
+            }
+        }
+    }
+    
     clear() {
         this.#length = 0;
         this.#head = null;
         this.#tail = null;
     }
-
+    
     toString() {
         return JSON.stringify({
             length: this.length,
@@ -100,12 +118,30 @@ const stack = new Stack();
 
 stack.push(1);
 stack.push(2);
+stack.push(3);
+stack.push(4);
 stack.push(5);
+stack.push(6);
 
 console.log(stack.toString())
 
-stack.pop();
-stack.pop();
-stack.pop();
+const lastNode = stack.pop();
+const secondLastNode = stack.pop();
+const thirdLastNode = stack.pop();
 
+console.log(lastNode, secondLastNode, thirdLastNode);
+
+console.log(stack.toString())
+
+const iterate = stack.iterator()
+console.log(iterate.next())
+console.log(iterate.next())
+console.log(iterate.next())
+console.log(iterate.next())
+console.log(iterate.next())
+console.log(iterate.next())
+console.log(iterate.next())
+console.log(iterate.next())
+
+stack.clear();
 console.log(stack.toString());
